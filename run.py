3f979ca2b7810d58e436886060cc0840cf64db91
Daniel Callaghan <dan_cal89@hotmail.com>
2021-09-23 14:11:04 +0000

    add welcome message message to be called whenever user wants to start a game - It will greet the user and check for input validation before allowing user to continue

--- run.py
+++ run.py
@@ -2,3 +2,24 @@ import numpy as np
 from termcolor import colored 
 import requests 
 
+def welcome_message():
+  print("Welcome to the Sudoku app!")
+  print()
+  print("To play, first select the level of difficulty")
+  print()
+  print("Easy: 1 ~ Medium: 2 ~ Hard: 3")
+  print()
+  # While loop for input validation - user must input 1, 2 or 3
+  user_choice = input("please enter 1, 2 or 3: ")
+  expected_input = ["1", "2", "3"]
+  while user_choice not in expected_input:
+      print("Sorry, to continue, you must enter 1 for easy, 2 for medium or 3 for hard: ")
+      print()
+      user_choice = input("please enter 1, 2 or 3: ")
+
+  user_choice = int(user_choice)  # turn into an integer to be used by get_grid function
+  print(f'you chose the number {user_choice}')
+  
+
+
+welcome_message()
